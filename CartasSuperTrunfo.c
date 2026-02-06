1e2fd787eb3435f630233e89d610721fb6e73cdd
pedrohhecht-create <pedroh.hecht@gmail.com>
2026-02-06 02:20:31 +0000

    Espero q vá

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+
     printf("Desafio SuperTrunfo!\n");
-    
-    printf("NÍVEL NOVATO")
+    printf("NÍVEL NOVATO\n");
 
-    printf("CARTA 1\n");
-
-
+    /* CARTA 1 */
+    printf("\nCARTA 1\n");
     char estado1 = 'A';
-    char codigo1 = "A01"
+    char codigo1[4] = "A01";
     char cidade1[50] = "Sao Paulo";
     int populacao1 = 12325000;
     float area1 = 1521.11;
     float PIB1 = 699.28;
-    int pontosturisticos1 = 50;
+    int pontos1 = 50;
 
     printf("Estado: %c\n", estado1);
     printf("Codigo: %s\n", codigo1);
@@ -22,19 +21,17 @@ int main(){
     printf("Populacao: %d\n", populacao1);
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhoes\n", PIB1);
-    printf("Pontos Turisticos: %d\n", pontosturisticos1);
+    printf("Pontos Turisticos: %d\n", pontos1);
 
-    
-    printf("CARTA 2\n");
-
-
+    /* CARTA 2 */
+    printf("\nCARTA 2\n");
     char estado2 = 'B';
-    char codigo[4] = "B02"
+    char codigo2[4] = "B02";
     char cidade2[50] = "Rio de Janeiro";
     int populacao2 = 6748000;
     float area2 = 1200.25;
     float PIB2 = 300.50;
-    int pontosturisticos2 = 30;
+    int pontos2 = 30;
 
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo2);
@@ -42,32 +39,16 @@ int main(){
     printf("Populacao: %d\n", populacao2);
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhoes\n", PIB2);
-    printf("Pontos Turisticos: %d\n", pontosturisticos2);
-    
+    printf("Pontos Turisticos: %d\n", pontos2);
 
-    printf("NÍVEL AVENTUREIRO")
+    /* NÍVEL AVENTUREIRO */
+    printf("\nNÍVEL AVENTUREIRO\n");
 
-    printf("carta 1:\n")
-    
-    printf("Calcular a Densidade Populacional 1")
-    
-    int populacao1 = 12325000
-    float area1 = 1521.11
+    float densidade1 = populacao1 / area1;
+    float densidade2 = populacao2 / area2;
 
-    densidade = ((float)populacao1/area1);
+    printf("Densidade Carta 1: %.2f hab/km²\n", densidade1);
+    printf("Densidade Carta 2: %.2f hab/km²\n", densidade2);
 
-    printf("A densidade é "f%", densidade);
- 
-    printf("carta 2:\n")
-    
-    printf("Calcular a Densidade Populacional 2")
-    
-    int populacao2 = 6748000
-    float area2 = 1200.25
-
-    densidade2 = ((float)populacao2/area2);
-
-    printf("A densidade é "f%", densidade2);
-    
-    return 0;   
-} 
+    return 0;
+}
